6b8aa6c214b91f4504e6aa3e5b7b7e9b30fb5e0d
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-05-05 17:29:11 -0500

    add

--- school/models/students.py
+++ school/models/students.py
@@ -10,23 +10,48 @@ class Students(models.Model):
     _inherit = 'mail.thread'
     _description = "students"
 
-    name = fields.Char(string='Name', required=True, tracking=True)
-    identification_type = fields.Selection([('cc','CC'),('ni','NI'),('ce','CE')], string="ID Type", tracking=True)
-    identification_number = fields.Char(string="Identification Number", tracking=True)
-    photo = fields.Binary(string='Photo', tracking=True)
-    dob = fields.Date(string="Date of Birth", tracking=True)
-    gender = fields.Selection([('m', 'Male'), ('f', 'Female'), ('o', 'Otro')], string='Gender')
+    name = fields.Char(
+        string='Name',
+        required=True,
+        tracking=True)
+    identification_type = fields.Selection([
+        ('cc','CC'),
+        ('ni','NI'),
+        ('ce','CE')], string="ID Type", tracking=True)
+    identification_number = fields.Char(
+        string="Identification Number",
+        tracking=True)
+    photo = fields.Binary(
+        string='Photo',
+        tracking=True)
+    dob = fields.Date(
+        string="Date of Birth",
+        tracking=True)
+    gender = fields.Selection([
+        ('m', 'Male'),
+        ('f', 'Female'),
+        ('o', 'Otro')], string='Gender')
     blood_group = fields.Selection(
         [('A+', 'A+'), ('B+', 'B+'), ('O+', 'O+'), ('AB+', 'AB+'),
          ('A-', 'A-'), ('B-', 'B-'), ('O-', 'O-'), ('AB-', 'AB-')],
         string='Blood Group')
-    nationality = fields.Many2one('res.country', string='Nacionality')
-    city_id = fields.Many2one('res.city', string="City")
-    attachment = fields.Binary(string="Attachment", tracking=True)
-    address = fields.Char(string="Address")
-    cellphone = fields.Char(string="Phone")
-    neighborhood = fields.Char(string="Neighborhood")
-    email = fields.Char(string="Email")
+    nationality = fields.Many2one(
+        'res.country',
+        string='Nacionality')
+    city_id = fields.Many2one(
+        'res.city',
+        string="City")
+    attachment = fields.Binary(
+        string="Attachment",
+        tracking=True)
+    address = fields.Char(
+        string="Address")
+    cellphone = fields.Char(
+        string="Phone")
+    neighborhood = fields.Char(
+        string="Neighborhood")
+    email = fields.Char(
+        string="Email")
 
 
     _sql_constraints = [
