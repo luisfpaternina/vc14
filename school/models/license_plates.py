ca1af20317b9a3ebc03e288dec42410b708a0636
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-05-05 18:07:30 -0500

    Update license_plates.py

--- school/models/license_plates.py
+++ school/models/license_plates.py
@@ -20,18 +20,47 @@ class LicensePlates(models.Model):
     _inherit = 'mail.thread'
     _description = "License plates"
 
-    name = fields.Char(string="Nombre", readonly=True, required=True, copy=False, tracking=True, default='New')
-    student_id = fields.Many2one('students', string="Student")
-    course = fields.Char(string="Course")
-    create_date = fields.Datetime('Create Date', tracking=True, default= fields.Datetime().now())
-    identification_number = fields.Char(string="Identification number", tracking=True, related="student_id.identification_number")
-    blood_group = fields.Selection(string="Blood Group",related="student_id.blood_group")
-    identification_type = fields.Selection(string="Identification Type", related="student_id.identification_type")
-    address = fields.Char(string="Address", related="student_id.address")
-    cellphone = fields.Char(string="Phone", related="student_id.cellphone")
-    neighborhood = fields.Char(string="Neighborhood", related="student_id.neighborhood")
-    gender = fields.Selection(string="Gender", related="student_id.gender")
-    email = fields.Char(string="Email", related="student_id.email")
+    name = fields.Char(
+        string="Nombre",
+        readonly=True,
+        required=True,
+        copy=False,
+        tracking=True,
+        default='New')
+    student_id = fields.Many2one(
+        'students',
+        string="Student")
+    course = fields.Char(
+        string="Course")
+    create_date = fields.Datetime(
+        'Create Date',
+        tracking=True,
+        default= fields.Datetime().now())
+    identification_number = fields.Char(
+        string="Identification number",
+        tracking=True,
+        related="student_id.identification_number")
+    blood_group = fields.Selection(
+        string="Blood Group",
+        related="student_id.blood_group")
+    identification_type = fields.Selection(
+        string="Identification Type",
+        related="student_id.identification_type")
+    address = fields.Char(
+        string="Address",
+        related="student_id.address")
+    cellphone = fields.Char(
+        string="Phone",
+        related="student_id.cellphone")
+    neighborhood = fields.Char(
+        string="Neighborhood",
+        related="student_id.neighborhood")
+    gender = fields.Selection(
+        string="Gender",
+        related="student_id.gender")
+    email = fields.Char(
+        string="Email",
+        related="student_id.email")
 
 
     #Secuencia para las matriculas de los estudiantes
