0d0b5806370c3763b5c9d1e0438acdef4f5afa1a
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-05-06 09:37:35 -0500

    Update license_plates.py

--- school/models/license_plates.py
+++ school/models/license_plates.py
@@ -90,18 +90,7 @@ class LicensePlates(models.Model):
         self.env['sale.order'].create(vals)
 
 
-    @api.onchange('student_id')
-    def get_students_records(self):
-        for rec in self:
-            lines = []
-            stu_obj = rec.env['students'].search([('name', '=', rec.student_id.name)],limit=1)
-            if stu_obj:
-                for s in stu_obj.course_line_ids:
-                    vals = (0, 0,{
-                        'course_id': s.course_id.id
-                    })
-                    lines.append(vals)
-                rec.course_line_ids = lines
+    
 
 
 
